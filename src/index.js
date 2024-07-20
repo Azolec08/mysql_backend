b1bd23b42605a17fb7956dce22967503219267e2
Mark Celoza <markangeloceloza380@gmail.com>
2024-07-21 01:59:41 +0800

    chore:update files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mysql from "mysql2";
+import serverless from "serverless-http";
 
 dotenv.config();
 const app = express();
+const router = express.Router();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -17,12 +19,11 @@ app.use(express.json());
 app.use(cors());
 
 // Serve static files from the images directory
-
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.json("Hello this is the backend");
 });
 
-app.post("/books", (req, res) => {
+router.post("/books", (req, res) => {
   const q = "INSERT INTO books (`title`,`desc`, `cover`, `price`) VALUES (?)";
   const values = [
     req.body.title,
@@ -40,7 +41,7 @@ app.post("/books", (req, res) => {
   });
 });
 
-app.get("/books", (req, res) => {
+router.get("/books", (req, res) => {
   const q = "SELECT * FROM books";
   db.query(q, (err, data) => {
     if (err) return res.json(err);
@@ -48,7 +49,7 @@ app.get("/books", (req, res) => {
   });
 });
 
-app.delete("/books/:id", (req, res) => {
+router.delete("/books/:id", (req, res) => {
   const bookId = req.params.id;
   const q = "DELETE FROM books WHERE id = ?";
 
@@ -58,7 +59,7 @@ app.delete("/books/:id", (req, res) => {
   });
 });
 
-app.put("/books/:id", (req, res) => {
+router.put("/books/:id", (req, res) => {
   const bookId = req.params.id;
   const q =
     "UPDATE books SET `title`= ?, `desc` = ?, `cover`= ?, `price` = ? WHERE id = ?";
@@ -76,6 +77,7 @@ app.put("/books/:id", (req, res) => {
   });
 });
 
-app.listen(8000, () => {
-  console.log("Connected to backend");
-});
+app.use("/api", router);
+
+// Export the app as a serverless function
+export default serverless(app);
