f04419ac8c1d81b32bc63b16e7064c1453d3b43a
Mark Celoza <markangeloceloza380@gmail.com>
2024-07-28 13:35:27 +0800

    feat: set connection timeout

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000,
 });
 
 app.use(express.json());
